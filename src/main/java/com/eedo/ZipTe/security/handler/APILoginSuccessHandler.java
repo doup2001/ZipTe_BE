d2f0367dc39eca6c46904ce971e01460f0127298
eedo_y <doup2001@naver.com>
2024-09-27 00:50:25 +0900

    feat-login : Security 설정 / 성공 및 실패 handler 설정

---
+++ src/main/java/com/eedo/ZipTe/security/handler/APILoginSuccessHandler.java
@@ -0,0 +1,39 @@
+package com.eedo.ZipTe.security.handler;
+
+import com.eedo.ZipTe.domain.dto.MemberDTO;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+@Log4j2
+public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.info("-------Success Handler------");
+        log.info(authentication);
+
+        MemberDTO memberDTO = (MemberDTO) authentication.getPrincipal();
+
+        Map<String, Object> claims = memberDTO.getClaims();
+        claims.put("accessToken", "");
+        claims.put("refreshToken", "");
+
+        //JSon문자열로
+        Gson gson = new Gson();
+        String jsonStr = gson.toJson(claims);
+        response.setContentType("application/json;charset=UTF-8");
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write(jsonStr);
+        printWriter.close();
+    }
+}
