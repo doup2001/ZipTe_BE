7674802a32d3ca00f3e0dd0ef2dfa8ee0d2da74e
eedo_y <doup2001@naver.com>
2024-09-26 21:46:30 +0900

    main : basic-setting ✅

---
+++ src/main/java/com/eedo/ZipTe/ZipTeApplication.java
@@ -0,0 +1,13 @@
+package com.eedo.ZipTe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipTeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipTeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eedo/ZipTe/ZipTeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eedo.ZipTe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipTeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
